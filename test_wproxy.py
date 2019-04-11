b34e4ce1bdbab5fbddeb32e7bf21133a7f0d75de
dukov1975 <dukov@icloudpocket.ru>
2019-04-11 15:43:54 +0300

    Education PY23

--- test_wproxy.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-json_dict = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-currency_list = json_dict['Valute']
-for item in currency_list:
-    print(currency_list[item])
