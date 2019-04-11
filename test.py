221d026d2cb2134c25b02aa257927f976289eb2d
dukov1975 <dukov@icloudpocket.ru>
2019-04-11 15:35:11 +0300

    Education PY23

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import requests
 
-currency_list = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-print(currency_list)
+proxy = {'http': 'http://proxy1.lab.test:3128',
+         'https': 'https://proxy1.lab.test:3128'}
+
+json_dict = requests.get('https://www.cbr-xml-daily.ru/daily_json.js', proxies=proxy).json()
+currency_list = json_dict['Valute']
+for item in currency_list:
+    print(currency_list[item])
